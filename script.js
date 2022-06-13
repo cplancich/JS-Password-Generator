8002f914b0751f11f533fb8ec30f2473f180e768
colinplancich <colin.plancich@gmail.com>
2022-06-13 15:41:47 -0700

    Small cleanup / changes made

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Arrays for character types (numbers, spec chars, upper and lowercase alpha)
+// Arrays for character types (special characters, numbers, upper and lowercase letters)
 var specChars = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "[", "]", "{", "}", "|", ";", ":", "'", ",", ".", "/", "<", ">", "?", "`", "~"];
 var numChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var lowerChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -10,11 +10,11 @@ function generatePassword() {
   var pwdLength; 
   
   pwdLength = window.prompt("Please enter the desired length of your password (More than 8 characters, less than 128)");
-
   // Verify user input for password length is valid according to predefined criteria
-  // if(pwdLength < 8 || pwdLength > 128) {
-  // alert("You must choose a password length greater than 8 characters and less than 128 in length.");
-  // }
+  if(pwdLength < 8 || pwdLength > 128) {
+  alert("You must choose a password length greater than 8 characters and less than 128 in length.");
+  // Something needs to prevent next prompts from appearing until a valid number is entered
+  }
 
   // Prompt user for if they want special characters & store it in a variable
   useSpec = window.confirm("Would you like to use special characters?");
@@ -24,8 +24,9 @@ function generatePassword() {
   useLower = window.confirm("Would you like to use lower case letters?");
   // Prompt the user if they want upper case letters and store it in a variable
   useUpper = window.confirm("Would you like to use upper case letters?");
+  
   // Validate that at least one character choice is true
-  if( !pwdLength && !specBank && !numBank && !lowerBank && !upperBank) {
+  if( !useSpec && !useNum && !useLower && !useUpper ) {
     window.alert("You must select at least one character set to generate a password.");
   }
 
@@ -33,15 +34,13 @@ function generatePassword() {
   var userChars = [];
   // concat things into new container array
   
-  // for(var i=0; i < passwordLength; i++)
+  // for(var i=0; i < pwdLength; i++)
     
     // Randomly select characters
     // Adding character to a building password string
     // RETURN the completed string
 
 }
-
-
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
