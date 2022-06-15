fefbb62fc1434bb343db4138828972a0955c77f1
colinplancich <colin.plancich@gmail.com>
2022-06-14 17:09:43 -0700

    Small changes, added Todo's

--- script.js
+++ script.js
@@ -10,35 +10,38 @@ function generatePassword() {
   var pwdLength; 
   
   pwdLength = window.prompt("Please enter the desired length of your password (More than 8 characters, less than 128)");
-  // Verify user input for password length is valid according to predefined criteria
-  if(pwdLength < 8 || pwdLength > 128) {
-  alert("You must choose a password length greater than 8 characters and less than 128 in length.");
+
+  // TODO: Verify user input for password length is valid according to predefined criteria
+  // if(pwdLength < 8 || pwdLength > 128) {
+  // alert("You must choose a password length greater than 8 characters and less than 128 in length.");
   // Something needs to prevent next prompts from appearing until a valid number is entered
-  }
+  // }
 
   // Prompt user for if they want special characters & store it in a variable
-  useSpec = window.confirm("Would you like to use special characters?");
+  var useSpec = window.confirm("Would you like to use special characters?");
   // Prompt the user if they want numbers and store it in a variable
-  useNum = window.confirm("Would you like to use numbers?");
+  var useNum = window.confirm("Would you like to use numbers?");
   // Prompt the user if they want lower case letters and store it in a variable
-  useLower = window.confirm("Would you like to use lower case letters?");
+  var useLower = window.confirm("Would you like to use lower case letters?");
   // Prompt the user if they want upper case letters and store it in a variable
-  useUpper = window.confirm("Would you like to use upper case letters?");
+  var useUpper = window.confirm("Would you like to use upper case letters?");
   
-  // Validate that at least one character choice is true
-  if( !useSpec && !useNum && !useLower && !useUpper ) {
-    window.alert("You must select at least one character set to generate a password.");
-  }
+  // TODO: Validate that at least one character choice is true
+  // if( !useSpec && !useNum && !useLower && !useUpper ) {
+  //   window.alert("You must select at least one character set to generate a password.");
+  // }
 
   // Declare a new empty array to contain user input data
-  var userChars = [];
-  // concat things into new container array
+  var selectedChars = [];
+  // TODO: concat things into new container array
   
   // for(var i=0; i < pwdLength; i++)
     
-    // Randomly select characters
-    // Adding character to a building password string
-    // RETURN the completed string
+    // TODO: Randomly select characters
+
+    // TODO: Add characters to a password container string
+
+    // TODO: RETURN the completed string
 
 }
 
